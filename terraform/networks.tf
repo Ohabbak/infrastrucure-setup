7f16b26a6843aa0f7fcf4b5e352cae8955aaa7b9
Ohabbak <habbakomar27@gmail.com>
2024-10-16 02:44:40 +0300

    jenkins server3

--- terraform/networks.tf
+++ terraform/networks.tf
@@ -2,7 +2,7 @@
 resource "aws_subnet" "public_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"  # Change as needed
+  availability_zone = "us-west-1a"  # Change as needed
 
   tags = {
     Name = "public_subnet"
@@ -13,7 +13,7 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1a"  # Change as needed
+  availability_zone = "us-west-1a"  # Change as needed
 
   tags = {
     Name = "private_subnet"
