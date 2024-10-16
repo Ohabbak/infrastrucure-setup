60fd05027014db7f1ef002c7b935a2d7b97130a4
Ohabbak <habbakomar27@gmail.com>
2024-10-16 03:10:19 +0300

    jenkins server-permissions2

--- jumphost-setup.sh
+++ jumphost-setup.sh
@@ -6,7 +6,7 @@ sudo cp ec2.pem ~/.ssh
 
 bastion_ip=$(terraform output --raw bastion_public_ip)
 private_ip=$(terraform output --raw Private_instance_ip)
-cat << EOF > ~/.ssh/config
+sudo cat << EOF > ~/.ssh/config
 Host private
     HostName ${private_ip}
     ProxyCommand ssh -W %h:%p bastion
@@ -21,7 +21,7 @@ Host bastion
     IdentityFile ~/.ssh/ec2.pem
 EOF
 
-cat << EOF > ../ansible/vars.yml
+sudo cat << EOF > ../ansible/vars.yml
 private_ec2_ip: "${private_ip}"  # Replace with actual private EC2 IP
 app_port: 30010  # Replace with actual application port
 EOF
