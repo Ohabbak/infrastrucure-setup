6b4a47b64c7377af1b76a2ef761e7a0b9ce21219
Ohabbak <habbakomar27@gmail.com>
2024-10-18 00:09:10 +0300

    remove sudo access from jumbhost script

--- jumphost-setup.sh
+++ jumphost-setup.sh
@@ -1,12 +1,12 @@
 cd terraform
-sudo chmod 400 ec2.pem
-sudo rm ~/.ssh/ec2.pem
-sudo cp ec2.pem ~/.ssh
+chmod 400 ec2.pem
+rm ~/.ssh/ec2.pem
+cp ec2.pem ~/.ssh
 
 
 bastion_ip=$(terraform output --raw bastion_public_ip)
 private_ip=$(terraform output --raw Private_instance_ip)
-sudo cat << EOF > ~/.ssh/config
+cat << EOF > ~/.ssh/config
 Host private
     HostName ${private_ip}
     ProxyCommand ssh -W %h:%p bastion
@@ -21,7 +21,7 @@ Host bastion
     IdentityFile ~/.ssh/ec2.pem
 EOF
 
-sudo cat << EOF > ../ansible/vars.yml
+cat << EOF > ../ansible/vars.yml
 private_ec2_ip: "${private_ip}"  # Replace with actual private EC2 IP
 app_port: 30010  # Replace with actual application port
 EOF
