eb1154d5762fe309f3b6eeda678eb121bea01802
Ohabbak <habbakomar27@gmail.com>
2024-10-18 02:34:38 +0300

    Lookup private_ip from the enviroment

--- terraform/networks.tf
+++ terraform/networks.tf
@@ -100,6 +100,13 @@ resource "aws_security_group" "private_sg" {
     cidr_blocks = ["0.0.0.0/0"]  # NodePort access (for Kubernetes services)
   }
 
+  ingress {
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # NodePort access (for Kubernetes services)
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
