709fcc7a30a6cc307645096d8963c4a37f561a49
Ohabbak <habbakomar27@gmail.com>
2024-10-19 00:44:56 +0300

    Update jumphost-setup.sh

--- jumphost-setup.sh
+++ jumphost-setup.sh
@@ -3,7 +3,6 @@ chmod 400 ec2.pem
 rm ~/.ssh/ec2.pem
 cp ec2.pem ~/.ssh
 
-
 bastion_ip=$(terraform output --raw bastion_public_ip)
 private_ip=$(terraform output --raw Private_instance_ip)
 cat << EOF > ~/.ssh/config
